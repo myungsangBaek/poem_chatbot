15f5fe87678b517c5b5d6020de2e35ef8ddd27b2
baek-myungsang <134994786+baek-myungsang@users.noreply.github.com>
2024-09-09 18:13:43 +0900

    feat: qa chain 및 dictionary chain 추가

--- chat.py
+++ chat.py
@@ -1,10 +1,15 @@
 import streamlit as st
 from controller import get_ai_message
 
-st.set_page_config(page_title="음유시인 챗봇", page_icon="📖")
+from dotenv import load_dotenv
 
-st.title("🤖 음유시인 챗봇")
-st.caption("음유시인 챗봇은 주제를 입력하면 시를 창작하는 챗봇입니다.")
+
+load_dotenv()
+
+st.set_page_config(page_title="음유시봇", page_icon="📖")
+
+st.title("🤖 음유시봇")
+st.caption("음유시봇은 주제를 입력하면 당신만을 위한 시를 쓰는 챗봇입니다.")
 
 if 'message_list' not in st.session_state:
     st.session_state.message_list = []
@@ -14,13 +19,13 @@ for message in st.session_state.message_list:
         st.write(message["content"])
 
 
-if user_question := st.chat_input(placeholder="'OO을 주제로 시를 창작해줘!' 라고 말해주세요."):
+if user_question := st.chat_input(placeholder="'OO을 주제로 시를 창작해줘!' 라고 입력해주세요."):
     with st.chat_message("user"):
         st.write(user_question)
     st.session_state.message_list.append(
         {"role": "user", "content": user_question})
 
-    with st.spinner("시를 창작하는 중입니다..."):
+    with st.spinner("음유시봇이 당신만을 위한 시를 쓰는 중입니다..."):
 
         # AI 메세지 생성
         with st.chat_message("ai"):
--- controller.py
+++ controller.py
@@ -1,11 +1,61 @@
-from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+import os
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import RetrievalQA
+from langchain import hub
+from langchain_pinecone import PineconeVectorStore
+
+
+def get_retriever():
+
+    embedding = OpenAIEmbeddings(model='text-embedding-3-large')
+    index_name = 'poem-index'
+
+    database = PineconeVectorStore.from_existing_index(
+        index_name=index_name, embedding=embedding)
+    retriever = database.as_retriever(search_kwargs={'k': 4})
+    return retriever
+
+
+def get_llm(model='gpt-4o'):
+    llm = ChatOpenAI(model=model)
+    return llm
+
+
+def get_dictionary_chain():
+    dictionary = ["키워드를 읽고 해당 관련된 주제로 변환"]
+    llm = get_llm()
+    prompt = ChatPromptTemplate.from_template(f"""
+                                            사용자의 질문을 보고, 쓸데 없는 문장들은 제거하고 키워드를 찾아서 주제로 정할 수 없다고 판단되면, 다시 입력해달라고하고 
+                                            주제를 정하면 해당 주제에 맞는 시를 창작해주세요.
+                                            
+                                            사용자의 질문: {{user_message}}
+                                            주제: {dictionary}
+                                              """)
+
+    dictionary_chain = prompt | llm | StrOutputParser()
+    return dictionary_chain
+
+
+def get_qa_chain():
+    llm = get_llm()
+    retriever = get_retriever()
+    prompt = hub.pull('rlm/rag-prompt')
+
+    qa_chain = RetrievalQA.from_chain_type(
+        llm, retriever=retriever, chain_type_kwargs={'prompt': prompt})
+    return qa_chain
 
 
 def get_ai_message(user_message):
-    load_dotenv()
-    llm = ChatOpenAI(model='gpt-4o')
+    dictionary_chain = get_dictionary_chain()
+    qa_chain = get_qa_chain()
 
-    response = llm.invoke(user_message)
+    poem_chain = {"query": dictionary_chain} | qa_chain
+    ai_message = poem_chain.invoke({"user_message": user_message})
 
-    return response.content
+    content = ai_message["query"]
+    result = ai_message["result"]
+
+    return f"{content}\n\n 설명 : {result}"
