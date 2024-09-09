158440345b80135c4657017cfd02c0424f377766
baek-myungsang <134994786+baek-myungsang@users.noreply.github.com>
2024-09-09 19:07:36 +0900

    feat: 히스토리 관리 체인 추가

--- controller.py
+++ controller.py
@@ -2,8 +2,22 @@ import os
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-
+from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_pinecone import PineconeVectorStore
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+store = {}
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
 
 
 def get_retriever():
@@ -37,15 +51,19 @@ def get_dictionary_chain():
     return dictionary_chain
 
 
-def get_qa_chain():
+def get_rag_chain():
     llm = get_llm()
     retriever = get_retriever()
 
     contextualize_q_system_prompt = (
-        "Given a chat history and the latest user question"
+        "Given a chat history and the latest user question "
+        "which might reference context in the chat history, "
+        "formulate a standalone question which can be understood "
+        "without the chat history. Do NOT answer the question, "
+        "just reformulate it if needed and otherwise return it as is."
     )
 
-    contextualize_q_prompt = ChatPromptTemplate.from_message(
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
         [
             ("system", contextualize_q_system_prompt),
             MessagesPlaceholder("chat_history"),
@@ -56,17 +74,47 @@ def get_qa_chain():
     history_aware_retriever = create_history_aware_retriever(
         llm, retriever, contextualize_q_prompt)
 
-    return qa_chain
+    system_prompt = ("You are an assistant for question-answering tasks. "
+                     "Use the following pieces of retrieved context to answer"
+                     "the question. If you don't know the answer, say that you "
+                     "don't know. Use three sentences maximum and keep the "
+                     "answer concise."
+                     "\n\n"
+                     "{context}"
+                     )
+
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}")
+        ]
+    )
+
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+    rag_chain = create_retrieval_chain(
+        history_aware_retriever, question_answer_chain)
+
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key='input',
+        history_messages_key='chat_history',
+        output_messages_key='answer',
+    )
+
+    return conversational_rag_chain
 
 
 def get_ai_message(user_message):
     dictionary_chain = get_dictionary_chain()
-    qa_chain = get_qa_chain()
+    rag_chain = get_rag_chain()
 
-    poem_chain = {"query": dictionary_chain} | qa_chain
-    ai_message = poem_chain.invoke({"user_message": user_message})
+    poem_chain = {"input": dictionary_chain} | rag_chain
+    ai_message = poem_chain.invoke({"user_message": user_message},   config={
+                                   "configurable": {"session_id": "abc123"}},)
 
-    content = ai_message["query"]
-    result = ai_message["result"]
+    content = ai_message["input"]
+    answer = ai_message["answer"]
 
-    return f"{content}\n\n 설명 : {result}"
+    return f"{content}\n\n 설명 : {answer}"
